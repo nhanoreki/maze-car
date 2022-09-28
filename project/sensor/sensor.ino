d1e40a7b06fb1e830ef292fb3890b31a9b2fff94
nhanoreki <nanoreki@gmail.com>
2022-09-28 21:40:14 +0700

    build sensor

--- project/sensor/sensor.ino
+++ project/sensor/sensor.ino
@@ -1,9 +1,83 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <avr/interrupt.h>
 
+#define SOUND_SPEED 34320
+#define STEP_TIME_64 4e-6
+#define TIMER1_STEP_CYCLE 65536
+
+const byte trig = 8;
+const byte echoH = 9;
+const byte echoL = 10;
+const byte echoR = 11;
+
+
+volatile float distanceHead, distanceLeft, distanceRight;
+volatile unsigned long currentSensorHead = 0, currentSensorLeft = 0, currentSensorRight = 0;
+volatile byte allSensor = 0;
+volatile bool headHigh = false, leftHigh = false, rightHigh = false;
+volatile bool headIsRec = false;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(trig, OUTPUT);
+  pinMode(echoH, INPUT_PULLUP);
+  pinMode(echoL, INPUT_PULLUP);
+  pinMode(echoR, INPUT_PULLUP);
+  
+  cli();
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TIMSK1 = 0;
+  TCCR1B |= (1 << CS11) | (1 << CS10);
+  TCNT1 = 0;
+  TIMSK1 = (1 << TOIE1);
+  PCICR = 0;
+  PCMSK0 = 0;
+  PCICR |= (1 << PCIE0);
+  PCMSK0 |= (1 << PCINT1) | (1 << PCINT2) | (1 << PCINT3);
+  sei();
+  digitalWrite(trig, HIGH);
+}
+
+ISR (PCINT0_vect) {
+  if (digitalRead(echoH) != headHigh) 
+  {
+    if (headHigh){
+      distanceHead = SOUND_SPEED * (((TCNT1 - currentSensorHead + TIMER1_STEP_CYCLE) % TIMER1_STEP_CYCLE) * STEP_TIME_64) / 2;
+      ++allSensor;
+    } else
+    {
+      currentSensorHead = TCNT1;
+    }
+    headHigh = !headHigh;
+  }
+  if (digitalRead(echoL) != leftHigh) {
+    if (leftHigh) {
+      distanceLeft = SOUND_SPEED * (((TCNT1 - currentSensorLeft + TIMER1_STEP_CYCLE) % TIMER1_STEP_CYCLE) * STEP_TIME_64) / 2;
+      ++allSensor;
+    } else {
+      currentSensorLeft = TCNT1; 
+    }
+    leftHigh = !leftHigh;
+  }
+  if (digitalRead(echoR) != rightHigh) {
+    if (rightHigh) {
+      distanceRight = SOUND_SPEED * (((TCNT1 - currentSensorRight + TIMER1_STEP_CYCLE) % TIMER1_STEP_CYCLE) * STEP_TIME_64) / 2;
+      ++allSensor;
+    } else {
+      currentSensorRight = TCNT1; 
+    }
+    rightHigh = !rightHigh;
+  }
+  if (allSensor == 3) {
+    allSensor = 0;
+    digitalWrite(trig, HIGH);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  Serial.print(distanceHead);
+  Serial.print(" ");
+  Serial.print(distanceLeft);
+  Serial.print(" ");
+  Serial.println(distanceRight);
 }
